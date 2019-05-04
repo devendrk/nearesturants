7bc3c4ca5ed7350886dbc884d3998e7f48cb911d
Devendra <katwaldevendra@hotmail.com>
2019-05-04 21:05:07 +0300

    resturant menu appended in card's body

--- sortProduct.js
+++ sortProduct.js
@@ -1,49 +1,69 @@
-const root = document.querySelector('#root');
+const root = document.querySelector("#root");
 
-function createHtmlElement(element,attribute,value){
-    let  el = document.createElement(element);
-    el.setAttribute(attribute,value);
-    return el;  
+function createHtmlElement(element, attribute, value) {
+  let el = document.createElement(element);
+  el.setAttribute(attribute, value);
+  return el;
 }
+
+function cards(element) {
+    //card: div
+  let divCard = createHtmlElement("div", "class", "card bg-light mb-3");
+  divCard.style.cssText = style = "max-width: 18rem;";
+  root.appendChild(divCard);
+
+  // card: header div
+  let divCardHeader = createHtmlElement("div","class","card-header font-weight-bold");
+  divCardHeader.textContent = element.name;
+  divCard.appendChild(divCardHeader);
+
+  //card: body & title div
+  let divCardBody = createHtmlElement("div", "class", "card-body");
+  let h5CardTitle = createHtmlElement("h5", "class", "card-title");
+  h5CardTitle.textContent = "Todays Menu";
+  divCard.appendChild(divCardBody);
+  divCardBody.appendChild(h5CardTitle);
+
+  // card: menu list 
+  let ulCard = createHtmlElement("ul", "class", "card-text ");
+  let li= createHtmlElement("li", "class", "card-text list-unstyled");
+  let lunchArray= element.lunches
+  if(lunchArray.length>0){
+      lunchArray.forEach(lunch=>{
+        li.textContent= lunch.title;  
+        ulCard.appendChild(li)
+        divCardBody.appendChild(ulCard);
+    })
+}else{
+    
+    li.textContent= '';
+    ulCard.appendChild(li)
+    divCardBody.appendChild(ulCard);
+  }
+//   console.log(lunchArray)
+}
+
 // Set up our HTTP request
 let xhr = new XMLHttpRequest();
-xhr.onload = function(){
-    // process return data
-    if(this.readyState===4){
-        if(xhr.status>=200 && xhr.status <300){
-            // this will run when the request is successful
-            let data = JSON.parse(xhr.responseText).restaurants;
-            data.forEach(element => {
-                // console.log(element.name
-                let divCard = createHtmlElement('div','class','card bg-light mb-3')
-            divCard.style.cssText =  style="max-width: 18rem;"; 
-            let divCardHeader = createHtmlElement('div','class','card-header font-weight-bold');
-            divCardHeader.textContent = element.name;
-            let divCardBody = createHtmlElement('div','class','card-body');
-            let pCardText = createHtmlElement('p','class','card-text ');
-            pCardText.textContent = 'this will be dynamic list of meal'
-            divCardBody.appendChild(pCardText);
-            divCard.appendChild(divCardHeader);
-            divCard.appendChild(divCardBody);
-            root.appendChild(divCard);
-
-            });
-            console.log(data[0].name)
-            
-        }
-    }else {
-        console.log('request failed');
+xhr.onload = function() {
+  // process return data
+  if (this.readyState === 4) {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // this will run when the request is successful
+      let data = JSON.parse(xhr.responseText).restaurants;
+      data.forEach(cards);
     }
+  } else {
+    console.log("request failed");
+  }
 
-    console.log('this will awlays runs');
-}
+};
 
-// create and send a GET request
 
-//================IN CASE OF ERROR LIKE BELOW==============
+/*================IN CASE OF ERROR LIKE BELOW==============
 // ADD  THIS BEFORE URL: 'https://cors.io/?HTTP://URL
 //No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://internet.derp' is therefore not allowed access.
-
-
-xhr.open('GET','https://cors.io/?http://www.lolnas.fi/api/restaurants.json');
+*/
+// create and send a GET request
+xhr.open("GET", "https://cors.io/?http://www.lolnas.fi/api/restaurants.json");
 xhr.send();
